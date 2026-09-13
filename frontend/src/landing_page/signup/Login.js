b8948366d88f5bbf646bf25f22cac4019e38f75e
MahimaJethva <mahimajethva@gmail.com>
2026-09-13 23:29:26 +0530

    Change little

--- frontend/src/landing_page/signup/Login.js
+++ frontend/src/landing_page/signup/Login.js
@@ -31,7 +31,7 @@ function Login() {
         localStorage.setItem("token", data.token);
         alert(data.message || "Login Successful!");
         // Exact Dashboard Port 3001 Redirect
-        window.location.href = "https://a-stock-trading-platform-zerodha.vercel.app";
+        window.location.href = "https://a-stock-trading-platform-zerodha-yb-two.vercel.app";
       } else {
         alert(data.message || "Login failed");
       }
